63f441e4a5f9bd303c32b8bd7aad05247ca02d50
sarmadi <mr.snaros@gmail.com>
2021-05-08 15:40:30 +0430

    remove comment

--- flask/models/User.py
+++ flask/models/User.py
@@ -33,29 +33,4 @@ class Users(db.Model):
         return Users.query.get(data['id'])
 
 
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(32), index=True)
-#     password_hash = db.Column(db.String(128))
-
-#     def hash_password(self, password):
-#         self.password_hash = generate_password_hash(password)
-
-#     def verify_password(self, password):
-#         return check_password_hash(self.password_hash, password)
-
-#     def generate_auth_token(self, expires_in=600):
-#         return jwt.encode(
-#             {'id': self.id, 'exp': time.time() + expires_in},
-#             app.config['SECRET_KEY'], algorithm='HS256')
-
-#     @staticmethod
-#     def verify_auth_token(token):
-#         try:
-#             data = jwt.decode(token, app.config['SECRET_KEY'],
-#                               algorithms=['HS256'])
-#         except:
-#             return
-#         return User.query.get(data['id'])
 
